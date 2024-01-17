7b359ba5436f7abf484e7c328048aac9aab179ca
neyha06 <130225539+neyha06@users.noreply.github.com>
2024-01-17 19:25:24 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Random r = new Random();
+        Random rand = new Random();
         int guess = 0;
-        int number = 1 + r.nextInt(100);
+        int number = 1 + rand.nextInt(100); // generate random number between 1 to 100
         int tries = 1;
-        System.out.println("I have chosen a number between 1-100. Try to guess it.");
-        System.out.print("Your Guess: ");
+        System.out.println("I have chosen a number between 1-100. Try to guess it. BEST OF LUCK!");
+        System.out.print("Enter your Guess: ");
         guess = input.nextInt();
 
         do {
-            System.out.println("That's incorrect. Try Again");
-            System.out.print("Your Guess: ");
+            System.out.println(" Ophs! that's incorrect! Try Again");
+            System.out.print("Enter your Guess: ");
             guess = input.nextInt();
             tries += 1;
         } while (guess != number);
